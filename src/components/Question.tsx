acd4d137de60a8efac317f75241161c8dc370283
minhtriqn05 <minhtriqn05@gmail.com>
2025-05-13 14:12:25 +0700

    Question responsive
    Thêm cái khung tròn tên tỉnh và hiệu ứng responsive cho question

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -8,10 +8,19 @@ const QuestionTem: React.FC = () => {
     return (
         <div className="flex justify-center min-h-[calc(100vh-128px)] bg-cyan-500">
             <form className="w-full h-full p-8">
-                <div className="bg-opacity-30 bg-sky-100 p-8 pb-10 mx-6 rounded-[30px] mx-auto my-2 border-4 border-sky-300 flex flex-col">
-                    <div className="text-left mb-4">
+              <div className="bg-opacity-30 bg-sky-100 p-8 pb-10 mx-6 rounded-[30px] mx-auto my-2 border-4 border-sky-300 flex flex-col relative">
+                        {/* Thành phần hiển thị tên tỉnh */}
+                        <div className="absolute top-[-3px] left-[-1px] bg-orange-500 text-white font-bold px-3 sm:px-4 md:px-6 py-2 rounded-[25px] shadow-lg border-[3px] border-orange-700 whitespace-nowrap overflow-hidden text-sm sm:text-base md:text-lg max-w-[90%] transition-all duration-300">
+                                🏖️ Vung Tau Province
+                        </div>
+
+
+    
+
+                    <div className="text-center mb-4">
                         <h3 className="text-xl font-semibold">Which beach is this?</h3>
                     </div>
+
                     <div className="flex-grow flex items-center justify-center">
                         <img 
                             src={vt} 
@@ -46,7 +55,6 @@ const QuestionTem: React.FC = () => {
                         </button>
                     </div>
                 </div>
-
             </form>
         </div>
     );
