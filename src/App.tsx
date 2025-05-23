6a788305092a6ad9962ba0e3735d9330e1fe8b53
Khiem <24560010@gm.uit.edu.vn>
2025-05-23 11:37:12 +0700

    addedquestions

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TalkingAnt from './components/TalkingAnt';
 import Home from './pages/Home';
 import Performance from './pages/Performance';
 import Review from './pages/Review';
@@ -37,6 +38,7 @@ const App: React.FC = () => {
              </Routes>
           </div>
         </main>
+        <TalkingAnt />
         <Footer />
       </div>
     </Router>
