7d9590aaf32db93f2e21ff9252d41f96aaaad12d
Khiem <24560010@gm.uit.edu.vn>
2025-06-05 23:19:32 +0700

    idktryingtofixconnection

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
+    API_URL: process.env.NODE_ENV === 'production' 
+        ? window.location.origin // Uses the same domain as your frontend
+        : 'http://localhost:3001'
 };
 
 export default config;
